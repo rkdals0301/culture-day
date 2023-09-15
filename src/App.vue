45d357f737913b482837f6f2ed54f95bd498c7f4
Choi minyoung <minyoung@Choiui-MacBookAir.local>
2023-09-16 01:53:13 +0900

    Add map

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <div>
+  <div id="app">
     <snackbar></snackbar>
+    <Map></Map>
   </div>
 </template>
 
 <script>
-import Snackbar from '@/components/common/Snackbar.vue'
+import Snackbar from "@/components/common/Snackbar.vue";
+import Map from "@/views/map/Map.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Snackbar
+    Snackbar,
+    Map,
   },
   data() {
-    return {
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import "./assets/css/reset.css";
 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+}
 </style>
---
+++ src/views/map/Map.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="map" style="width: 100%; height: 100%"></div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+
+  mounted() {
+    this.init();
+  },
+
+  methods: {
+    init() {
+      // load naver maps API
+      const script = document.createElement("script");
+      const mapUrl = process.env.VUE_APP_MAP_URL;
+      const key = process.env.VUE_APP_MAP_CLIENT_ID;
+
+      script.src = mapUrl + "=" + key;
+      script.async = true;
+      script.defer = true;
+      document.head.appendChild(script);
+
+      script.onload = () => {
+        // create naver maps
+        new window.naver.maps.Map("map", {
+          center: new window.naver.maps.LatLng(37.5670135, 126.978374),
+          zoom: 14,
+        });
+      };
+    },
+  },
+};
+</script>
+
+<style scoped></style>
