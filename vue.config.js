afb6282b5741e43a8768135082b724a8bcb0a88e
이강민 <rkdals0301@naver.com>
2023-12-12 16:31:26 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: '/culture-day',
+    publicPath: process.env.NODE_ENV === 'production' ? '/culture-day' : '/',
+    outputDir: './docs',
     transpileDependencies: [],
     css: {
         loaderOptions: {
