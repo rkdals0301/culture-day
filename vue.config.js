072a8b445264ed5644f0c637d25b8825b59d3fda
이강민 <rkdals0301@naver.com>
2023-12-12 16:19:19 +0900

    add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+    publicPath: '/culture-day',
     transpileDependencies: [],
     css: {
         loaderOptions: {
