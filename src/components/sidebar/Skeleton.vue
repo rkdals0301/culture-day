001d6aaab7d28f11d0f2b8bdd35e7b000b5cd191
rkdals0301 <rkdals0301@naver.com>
2024-09-02 04:01:18 +0900

    update Skeleton

--- src/components/sidebar/Skeleton.vue
+++ src/components/sidebar/Skeleton.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="skeleton-wrapper">
     <div v-for="skeleton of skeletons" :key="skeleton.uuid" class="skeleton">
-      <content-loader width="370" height="150" primary-color="#3A3A3A" secondary-color="#494949" :animate="true">
-        <rect x="0" y="0" rx="3" ry="3" width="260" height="25" />
-        <rect x="0" y="30" rx="3" ry="3" width="243" height="20" />
-        <rect x="0" y="55" rx="3" ry="3" width="190" height="20" />
-        <rect x="0" y="80" rx="3" ry="3" width="210" height="20" />
-        <rect x="0" y="105" rx="3" ry="3" width="90" height="20" />
-        <rect x="0" y="130" rx="3" ry="3" width="180" height="20" />
-        <rect x="270" y="0" rx="3" ry="3" width="100" height="155" />
+      <content-loader
+        width="370"
+        height="100"
+        primaryColor="#333333"
+        secondaryColor="#666666"
+        :animate="true"
+        :speed="3"
+      >
+        <rect x="0" y="0" rx="3" ry="3" width="260" height="15" />
+        <rect x="0" y="25" rx="3" ry="3" width="243" height="10" />
+        <rect x="0" y="45" rx="3" ry="3" width="190" height="10" />
+        <rect x="0" y="65" rx="3" ry="3" width="210" height="10" />
+        <rect x="0" y="85" rx="3" ry="3" width="90" height="10" />
+        <rect x="0" y="105" rx="3" ry="3" width="180" height="10" />
+        <rect x="270" y="0" rx="7" ry="7" width="100" height="100" />
       </content-loader>
     </div>
   </div>
@@ -39,8 +46,10 @@ export default {
 <style lang="scss" scoped>
 .skeleton-wrapper {
   .skeleton {
-    padding: 10px;
-    padding-top: 0;
+    background-color: $item_background_color;
+    border-radius: 7px;
+    padding: 15px;
+    margin: 0 10px 10px 10px;
   }
 }
 </style>
